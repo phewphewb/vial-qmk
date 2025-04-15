06c1067f3e82f6e7957ec67ec89e0eee7b3d7fff
phewphewb <valentyn.patsera@gmail.com>
2025-04-14 23:00:59 -0400

    types

--- keyboards/svalboard/keybard.c
+++ keyboards/svalboard/keybard.c
@@ -3,7 +3,7 @@
 static uint16_t last_refresh_time = 0;
 static bool keybard_active = false;
 
-bool get_is_keybard_active() {
+bool get_is_keybard_active(void) {
     if (keybard_active && timer_elapsed(last_refresh_time) > REFRESH_TIMEOUT_MS) {
         keybard_active = false
         last_refresh_time = 0;
@@ -12,7 +12,7 @@ bool get_is_keybard_active() {
     return keybard_active;
 }
 
-void refresh_keybard_active_timer() {
+void refresh_keybard_active_timer(void) {
     keybard_active = true;
     last_refresh_time = timer_read();
 }
--- keyboards/svalboard/layout_indicator.c
+++ keyboards/svalboard/layout_indicator.c
@@ -3,20 +3,21 @@
 static uint16_t last_refresh_time = 0;
 static bool layout_indicator_active = false;
 
-bool get_is_layout_indicator_active() {
+bool get_is_layout_indicator_active(void) {
     if (layout_indicator_active && timer_elapsed(last_refresh_time) > REFRESH_TIMEOUT_MS) {
-        disable_layout_indicator();
+        layout_indicator_active = false;
+        last_refresh_time = 0;
     }
 
     return layout_indicator_active;
 }
 
-void refresh_layout_indicator_timer() {
+void refresh_layout_indicator_timer(void) {
     layout_indicator_active = true;
     last_refresh_time = timer_read();
 }
 
-void disable_layout_indicator() {
+void disable_layout_indicator(void) {
     layout_indicator_active = false;
     last_refresh_time = 0;
 }
