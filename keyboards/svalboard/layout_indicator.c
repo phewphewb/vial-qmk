24586064b7898fd4ced1869b03ea1280e7aeadee
phewphewb <valentyn.patsera@gmail.com>
2025-04-14 22:29:25 -0400

    implement layout indicator

--- keyboards/svalboard/layout_indicator.c
+++ keyboards/svalboard/layout_indicator.c
@@ -5,17 +5,18 @@ static bool layout_indicator_active = false;
 
 bool get_is_layout_indicator_active() {
     if (layout_indicator_active && timer_elapsed(last_refresh_time) > REFRESH_TIMEOUT_MS) {
-        layout_indicator_active = false
+        disable_layout_indicator();
     }
 
     return layout_indicator_active;
 }
 
-void enable_layout_indicator() {
+void refresh_layout_indicator_timer() {
     layout_indicator_active = true;
     last_refresh_time = timer_read();
 }
 
 void disable_layout_indicator() {
     layout_indicator_active = false;
+    last_refresh_time = 0;
 }
--- keyboards/svalboard/layout_indicator.h
+++ keyboards/svalboard/layout_indicator.h
@@ -1,8 +1,13 @@
 #include "quantum.h"
 
-#define SVAL_ACITVE_LAYER  0xA5
-#define REFRESH_TIMEOUT_MS 30000  // Client must refresh every 30 seconds
+#define SVAL_ACTIVE_LAYER_INDICATOR 0xA5
+#define REFRESH_TIMEOUT_MS 30000 // Client must refresh every 30 seconds
+
+enum active_layer_command_ids {
+    active_layer_push    = 0x01,
+    active_layer_refresh = 0x02,
+};
 
 bool get_is_layout_indicator_active(void);
-void enable_layout_indicator(void);
+void refresh_layout_indicator_timer(void);
 void disable_layout_indicator(void);
